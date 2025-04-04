dffa8515b1f62872ad4b62927100ed296583e58e
felipe-dpmedeiros <felipe.dellapaschoa@gmail.com>
2025-04-03 22:28:24 -0300

    xml pronto

---
+++ app/src/main/java/felipedpmedeiros/com/example/android_crypto_monitor/MainActivity.kt
@@ -0,0 +1,21 @@
+package felipedpmedeiros.com.example.android_crypto_monitor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import felipedpmedeiros.com.example.android_crypto_monitor.ui.theme.Android_crypto_monitorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
